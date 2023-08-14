7d8b865ec071ece081c7e6852b01c9fe27d48488
di4m0nds <ws.2000.sja@gmail.com>
2023-08-14 10:11:35 -0300

    [ViewTransition] explore rocket with smooth transitions

---
+++ src/data/rockets.ts
@@ -0,0 +1,54 @@
+export const rockets = [
+    {
+        id: "5e9d0d95eda69955f709d1eb",
+        name:"Falcon 1",
+        description:"The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.",
+        country: "Republic of the Marshall Islands",
+        company: "SpaceX",
+        cost_per_launch: 6700000,
+        active: false,
+        wikipedia: "https://en.wikipedia.org/wiki/Falcon_1",
+        flickr_images: [
+            "https://i.imgur.com/DaCfMsj.jpg"
+        ]
+    },
+    {
+        id: "5e9d0d95eda69973a809d1ec",
+        name:"Falcon 9",
+        description:"Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit.",
+        country: "United States",
+        company: "SpaceX",
+        cost_per_launch: 50000000,
+        active: true,
+        wikipedia: "https://en.wikipedia.org/wiki/Falcon_9",
+        flickr_images: [
+            "https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg"
+        ]
+    },
+    {
+        id: "5e9d0d95eda69974db09d1ed",
+        name:"Falcon Heavy",
+        description:"With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost.",
+        country: "United States",
+        company: "SpaceX",
+        cost_per_launch: 90000000,
+        active: true,
+        wikipedia: "https://en.wikipedia.org/wiki/Falcon_Heavy",
+        flickr_images: [
+            "https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg"
+        ]
+    },
+    {
+        id: "5e9d0d96eda699382d09d1ee",
+        name:"Starship",
+        description:"Starship and Super Heavy Rocket represent a fully reusable transportation system designed to service all Earth orbit needs as well as the Moon and Mars. This two-stage vehicle — composed of the Super Heavy rocket (booster) and Starship (ship) — will eventually replace Falcon 9, Falcon Heavy and Dragon.",
+        country: "United States",
+        company: "SpaceX",
+        cost_per_launch: 7000000,
+        active: false,
+        wikipedia: "https://en.wikipedia.org/wiki/SpaceX_Starship",
+        flickr_images: [
+            "https://live.staticflickr.com/65535/48954138922_9c42173f08_b.jpg"
+        ]
+    },
+]
---
+++ src/utils/consts.ts
@@ -0,0 +1,9 @@
+/**
+ * GLOBAL VARIABLES
+ */
+
+export const BASE_URL = "https://api.spacexdata.com/"
+
+export const CURRENT_VERSION = "v4"
+
+export const ROCKETS_ENDPOINT = "/rockets"
